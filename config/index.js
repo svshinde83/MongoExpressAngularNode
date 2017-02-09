118b10f1c71eab04c22dad6679ecc7cca96bd349
svshinde83 <svshinde83@gmail.com>
2017-02-10 10:32:50 +1100

    initial commit

---
+++ config/index.js
@@ -0,0 +1,10 @@
+var configValues = require('./config');
+
+module.exports = {
+    
+    getDbConnectionString: function() {
+        return 'mongodb://' + configValues.uname+':'+configValues.pwd
+        +'@ds147079.mlab.com:47079/mongo-express-angular-node';
+    }
+    
+}
